c38e4f6c6be79364515e2d451630099242cc263c
Nurudeen Ahmed <nurudeenmyeslf@gmail.com>
2023-05-11 15:21:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Amar Care (medical facility)
+### Video Demo:  <URL https://youtu.be/2uIt1HoU7HQ>
+
+### Inception
+write an introduction for a presentation about a website that provides healthcare solutions, online consultation, drug shopping and doorstep delivery
+#### I was thinking of my final project from week 0 of cs50 and I had a lot of ideas but not for one moment did an online medical facility come to my mind. I thought of cool things like games or social apps. But on the day I finished Pset 9 (finance), I went to a clinic because I felt nauseous and the doctor prescribed the same meds he prescribed for me a few weeks ago when I felt the same way and those drugs didn't work. I had to bring his attendtion to it before he prescribed a different drug. Immediately, I thought to myself, this could be better. There should be a database for patients that should be readily available to the doctor. But unfortunately, in Nigeria it doesn't exist.
+#### The ideas started coming to me naturally as I turned on my PC and started doing some research on creating an efficent medical service that can tackle most of the problems we face here and in the wider world.
+### Description
+#### This project relies heavily on it's data base, I always thought of database management as some boring, tedious redundant job but this project made me love it. I might have even enjoyed it more than anything else because of how much it made my job easier. So right from the start, you can register as a new user with your email address, a password and your shipping address. Your information is immediately updated in the database and it's like you have a file with the facility. After registering you can login. You don't need to be logged in to see the homepage. But when you're logged in, you have more previleges in the homepage. There's a navbar at the top of the home page to help you navigate between our pages. There is the company logo on the navbar which redirects you to the homepage anytime you click on it, there are also four other links, if you're not logged in, you'll see: Talk to a doctor, Shop, Register, Login. While if you're logged in, you'll see: Talk to a doctor, Shop, Cart and logout.
+#### Underneath the navbar, There is a bright carousel that also follows the theme of the entire page. it welcomes you to the page and tells you about the company in pictures. Below the carousel is a heading that tells you about the company in a few hundred words. There are also 4 top selling products from the shop on display. I can't overemphasize how much sql helped me in this project!. lastly on every page, you can click on link to our various socials.
+#### The next page is the consultation page and the doctors are broken into various categories, the idea is that you can instantly book an appointment through their assistants via a live video chat during working hours.
+#### The next page is the shop page, all the items are looped over from the database once again. You can search for items or buy items on the go. You can even decide to just add them to cart. Items added to cart are dropped from cart when you successfully purchase them and the items you purchase are subtracted from the stock in the database. That way, you can not order for more items than are available in the stock.
+#### You can decide to log out and just surf through the page if you don't want to be logged in.
+#### At the end of the day, this project is not the crazy game I had in mind since week 0, but it's a lot more wholesome and it actually tackles a real problem here and I really hope to implement This project in the nearest future. I might sell the idea to an existing medical service or even start up mine. I really feel a part of this project and I appreciate the experience. The project has a lot more room for improvement and I definitely plan on doin so. Thank you cs50
+
+
+
+
+
+
+
+
+# amar-care
---
+++ app.py
@@ -0,0 +1,191 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///pharmacy.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    all_products = db.execute("SELECT * FROM products LIMIT 4")
+    return render_template("index.html", all_products=all_products)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("email"):
+            return apology("Don't be sneaky! must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("Don't be sneaky! must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE email = ?",
+                          request.form.get("email"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid email and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+        print(session["user_id"])
+
+        flash("Logged in")
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    flash("Logged out")
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return apology("must provide email", 400)
+
+        # Ensure email doesn't exist
+        rows = db.execute("SELECT email FROM users")
+        print(rows)
+        emails = []
+        for row in rows:
+            emails.append(row["email"])
+        print(emails)
+        if request.form.get("email") in emails:
+            return apology("email already exists", 400)
+
+        # Ensure password was submitted
+        if not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Ensure  passwords don't match
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+
+        # INSERT the new user into users
+        hashed_password = generate_password_hash(request.form.get(
+            "password"), method='pbkdf2:sha256', salt_length=8)
+        created_email = request.form.get("email")
+        address = request.form.get("address")
+        db.execute("INSERT INTO users (email, hash, address) VALUES(:created_email, :hashed_password, :address)",
+                   created_email=created_email, hashed_password=hashed_password, address=address)
+
+        return redirect("/login")
+
+    else:
+        return render_template("register.html")
+
+
+@app.route("/consultation")
+def consultation():
+    return render_template("consultation.html")
+
+
+@app.route("/shop", methods=["GET", "POST"])
+def shop():
+    if request.method == "POST":
+        product = request.form.get("product")
+        product = "%" + product + "%"
+        results = db.execute(
+            "SELECT * FROM products WHERE name LIKE ?", product)
+        return render_template("searched.html", results=results)
+
+    all_products = db.execute("SELECT * FROM products")
+    return render_template("shop.html", all_products=all_products)
+
+
+@app.route("/order", methods=["GET", "POST"])
+@login_required
+def order():
+    if request.method == "POST":
+        product = request.form.get("product")
+        results = db.execute("SELECT * FROM products WHERE id = ?", product)
+        db.execute("INSERT INTO orders (userId, productId) VALUES(?, ?)", session["user_id"], int(product))
+        return render_template("order.html", results=results)
+    return render_template("order.html")
+
+
+@app.route("/success", methods=["GET", "POST"])
+@login_required
+def success():
+    if request.method == "POST":
+        product = request.form.get("product")
+        results = db.execute("SELECT * FROM products WHERE id = ?", product)
+        amount = request.form.get("amount")
+        available = results[0]["stock"]
+        stock = results[0]["stock"] - int(amount)
+        if int(amount) > stock:
+            error = "we only have " + str(available) + " units left"
+            return apology(error, 400)
+        db.execute("UPDATE products SET stock = ? WHERE id = ?", stock, product)
+        db.execute("DELETE FROM orders WHERE productId = ? AND userId = ?", product, session["user_id"])
+        flash("SUCCESS!")
+        return render_template("success.html", results=results)
+    return render_template("success.html")
+
+
+@app.route("/orders")
+@login_required
+def orders():
+    user_id = session["user_id"]
+    results = db.execute("SELECT DISTINCT * FROM orders JOIN products ON orders.productId=products.id WHERE userId = ?", user_id)
+    return render_template("orders.html", results=results)
+
